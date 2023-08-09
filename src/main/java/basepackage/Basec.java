cee452af72e5afaec5fac924ae44b86a2454963f
manoj <manucse1989@gmail.com>
2023-08-09 18:16:05 +0530

    added one test cases for error message

---
+++ src/main/java/basepackage/Basec.java
@@ -0,0 +1,36 @@
+package basepackage;
+
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Basec {
+	
+	public static WebDriver driver;
+	
+	public static void launchApplication() {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		 driver = new ChromeDriver(options);
+		 driver.get("https://studio.cucumber.io/users/sign_in");
+		 driver.manage().window().maximize();
+		 //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));	 
+	}
+		
+	// 5mina please
+	
+	public static void closeApplication() {
+		
+		driver.close();
+	}
+	
+}
+
